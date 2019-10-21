b158fb1a33f44d4df317be3f4d020246ba9a9a19
SolFly <SolFly86@gmail.com>
2019-10-21 13:56:36 +0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -25,18 +25,18 @@
 #ifndef XMRIG_VERSION_H
 #define XMRIG_VERSION_H
 
-#define APP_ID        "SQLBackup"
-#define APP_NAME      "SQLBackupAndFTP"
-#define APP_DESC      "Database Backup Tool"
-#define APP_VERSION   "4.1.3-beta"
-#define APP_DOMAIN    "mysql.com"
-#define APP_SITE      "www.mysql.com"
-#define APP_COPYRIGHT "Copyright (C) 2011-2019 SQL"
+#define APP_ID        "xmrig"
+#define APP_NAME      "XMRig"
+#define APP_DESC      "XMRig miner"
+#define APP_VERSION   "4.3.1-beta"
+#define APP_DOMAIN    "xmrig.com"
+#define APP_SITE      "www.xmrig.com"
+#define APP_COPYRIGHT "Copyright (C) 2016-2019 xmrig.com"
 #define APP_KIND      "miner"
 
 #define APP_VER_MAJOR  4
-#define APP_VER_MINOR  1
-#define APP_VER_PATCH  3
+#define APP_VER_MINOR  3
+#define APP_VER_PATCH  1
 
 #ifdef _MSC_VER
 #   if (_MSC_VER >= 1920)
